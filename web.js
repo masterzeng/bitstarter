5ad93e8a39966c1ab6750668d174b6f8176ad6f3
masterzeng <paulmiaozeng@gmail.com>
2013-08-08 04:32:53 +0000

    adding web.js

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(HTMLImport);
 }); // <--- here close the function call
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80800;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
@@ -28,7 +28,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80800;
 app.listen(port, function() {
   console.log("Listening on " + port);
 }); */
